f381099df6a5ce73219d9c407cd81e0cb544362b
mmonay24 <marabelle.monay@gmail.com>
2021-11-11 12:32:12 -0800

    editing Invite and SettingNavCont component

--- app/comps/Invite/index.js
+++ app/comps/Invite/index.js
@@ -1,12 +1,22 @@
 import react from 'react';
 import styled from 'styled-components';
 
+const MainCont = styled.div`
+display: flex;
+flex-direction: column;
+margin-top:10px;
+width:100%;
+
+`
+
 const Cont = styled.div`
 display: flex;
 flex-direction: column;
+margin:40px;
+margin-right: 130px;
 `
 const Head = styled.div`
-font-size: 27px;
+font-size: 25px;
 font-weight: 600;
 color: #1E1E1E;
 margin-bottom: 12px;
@@ -18,7 +28,8 @@ font-weight: 400;
 `
 const Copy = styled.button`
 display: flex;
-max-width: 200px;
+max-width: 205px;
+height: 70px;
 justify-content: center;
 align-items: center;
 background-color: #F6F6FE;
@@ -27,7 +38,8 @@ padding: 20px;
 border-radius: 6px;
 margin-top: 25px;
 position: relative;
-left: ${props=>props.position};
+// left: ${props=>props.position};
+left:500px;
 `
 const Img = styled.img`
 margin-right: 10px;
@@ -36,23 +48,43 @@ const ButtonTxt = styled.div`
 font-size: 23px;
 color: #7751E8;
 font-weight: 600;
-;
 `
+
+const Line = styled.hr`
+border-color: #CCCCCC;
+border-width: 0.5px;
+border-style: solid;
+width: 100%;
+`
+
 const Invite = ({
-head="Invite a member with a link ",
-text="Invite a member to join your team from another device. ",
-position="500px",
+// head="Invite a member with a link ",
+// text="Invite a member to join your team from another device. ",
+// position="500px",
 
 
 }) => {
-    return <Cont>
-        <Head className="opensans">{head}</Head>
-        <Text className="opensans">{text} </Text>
-        <Copy position={position}>
+    return <MainCont>
+    <Cont>
+        <Head className="opensans">Invite a member with a link</Head>
+        <Text className="opensans">Invite a member to join your team from another device.</Text>
+        <Copy>
         <Img src="/link.svg"></Img>
         <ButtonTxt className="opensans">Copy Link</ButtonTxt>
         </Copy>
     </Cont>
+    <Line/>
+    <Cont>
+        <Head className="opensans">Invite a member with a code</Head>
+        <Text className="opensans">Invite a member to join your team by sending a code.</Text>
+        <Copy>
+        <Img src="/Code.svg"></Img>
+        <ButtonTxt className="opensans">Copy Code</ButtonTxt>
+        </Copy>
+    </Cont>
+
+</MainCont>
+    
 }
 
 export default Invite;
