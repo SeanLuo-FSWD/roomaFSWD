3087229e771f1d5ce9f308f5c1c1338866255442
ChisakiNakamura <cnakamura1@my.bcit.ca>
2021-11-12 05:00:41 -0800

    connected go to community in members page

--- app/comps/Reminder/index.js
+++ app/comps/Reminder/index.js
@@ -1,5 +1,4 @@
 import react from 'react';
-
 import styled from 'styled-components';
 import RemindContent from '../RemindContent';
 import Completed_RemindContent from '../RemindContent/completed';
