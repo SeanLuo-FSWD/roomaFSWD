ff8b5b888eba626c0393a8378dc7eebfbaaf520e
Sean Luo <shawnluo415@gmail.com>
2021-11-20 17:23:51 -0800

    working on chat left

--- app/comps/ChatNav/index.js
+++ app/comps/ChatNav/index.js
@@ -1,104 +1,98 @@
-import React from 'react'
-import styled from 'styled-components'
+import React from "react";
+import styled from "styled-components";
 
 const NavCont = styled.div`
-display:flex;
-background-color: ${props=>props.bgcolor};
-width: 85%;
-height: 13%;
-border-radius: 16px;
-margin-top: ${props=>props.margintop};
-`
+  display: flex;
+  background-color: ${(props) => props.bgcolor};
+  width: 85%;
+  height: 13%;
+  border-radius: 16px;
+  margin-top: ${(props) => props.margintop};
+`;
 
 //Profile
 const ProfileCont = styled.div`
-flex: 1;
-display:flex;
-justify-content: center;
-align-items:center;
-margin-left: 10px;
-`
+  flex: 1;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  margin-left: 10px;
+`;
 const Img1 = styled.img`
-width: 55px;
-height: 55px;
-border-radius: 50%;
-border: 2px solid white;
-margin-left: 20px;
-
-`
+  width: 55px;
+  height: 55px;
+  border-radius: 50%;
+  border: 2px solid white;
+  margin-left: 20px;
+`;
 const Img2 = styled.img`
-width: 55px;
-height: 55px;
-border-radius: 50%;
-border: 2px solid white;
-position: relative;
-right: 25px;
+  width: 55px;
+  height: 55px;
+  border-radius: 50%;
+  border: 2px solid white;
+  position: relative;
+  right: 25px;
 
-display:${props=>props.display};
-`
+  display: ${(props) => props.display};
+`;
 
 //Info
 const InfoCont = styled.div`
-flex: 4;
-display:flex;
-flex-direction: column;
-justify-content: center;
-margin-left: ${props=>props.marginleft};
-`
+  flex: 4;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  margin-left: ${(props) => props.marginleft};
+`;
 const Name = styled.div`
-font-size: 20px;
-font-weight: 600;
-color:#3E3D3D;
-margin-bottom: 5px;
-
-`
+  font-size: 20px;
+  font-weight: 600;
+  color: #3e3d3d;
+  margin-bottom: 5px;
+`;
 const Msg = styled.div`
-font-size: 15px;
-color: #979797;
-
-`
+  font-size: 15px;
+  color: #979797;
+`;
 
 //Time
 const TimeCont = styled.div`
-flex:1;
-display:flex;
-justify-content: center;
-align-items:center;
-margin-right: 10px;
-
-`
+  flex: 1;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  margin-right: 10px;
+`;
 const Time = styled.div`
-font-size: 13px;
-color:#7E7E7E;
-`
+  font-size: 13px;
+  color: #7e7e7e;
+`;
 const ChatNav = ({
-  bgcolor="#FAFAFA",
-  display="flex",
-  marginleft="25px",
-  margintop="40px",
-  info="All Roommates (5)",
-  msg="Hello Everyone!",
-  
-  onClick=()=>{}  
-  
-  }) => {
+  bgcolor = "#FAFAFA",
+  display = "flex",
+  marginleft = "25px",
+  margintop = "40px",
+  chatterInfo = "All Roommates (5)",
+  msg = "Hello Everyone!",
+  user,
+  onClick = () => {},
+}) => {
+  return (
+    <NavCont onClick={onClick} bgcolor={bgcolor} margintop={margintop}>
+      <ProfileCont>
+        <Img1 src="/Avatar2.png" />
+        <Img2 display={display} src="/Avatar2.png" />
+      </ProfileCont>
 
-    return <NavCont   onClick={onClick} bgcolor={bgcolor} margintop={margintop}>
-                <ProfileCont>
-                <Img1 src="/Avatar2.png"/>
-                <Img2 display={display}src="/Avatar2.png"/>
-                
-
-                </ProfileCont>
-
-                <InfoCont marginleft={marginleft}>
-                    <Name className="opensans">{info}</Name>
-                    <Msg  className="opensans">{msg}</Msg>
-                </InfoCont>
-                <TimeCont>
-                    <Time className="opensans">5 min</Time>
-                </TimeCont>
-            </NavCont>   
-}
+      <InfoCont marginleft={marginleft}>
+        <Name className="opensans">{chatterInfo.name}</Name>
+        <Msg className="opensans">Hello {user.name}</Msg>
+      </InfoCont>
+      <TimeCont>
+        <Time className="opensans">5 min</Time>
+      </TimeCont>
+    </NavCont>
+  );
+};
 
 export default ChatNav;
--- app/pages/chat.js
+++ app/pages/chat.js
@@ -1,22 +1,21 @@
-import Head from 'next/head'
-import styled from 'styled-components';
-import * as React from 'react';
-import NavBar3 from '../comps/NavBar3';
-import ChatNav from '../comps/ChatNav';
-import {useState} from 'react';
-import User1 from '../comps/ChatComps/user1'
-import User2 from '../comps/ChatComps/user2';
-import Input from '../comps/ChatComps/input';
-
-
+import { useState, useContext } from "react";
+import * as React from "react";
+import Head from "next/head";
+import styled from "styled-components";
+import NavBar3 from "../comps/NavBar3";
+import ChatNav from "../comps/ChatNav";
+import User1 from "../comps/ChatComps/user1";
+import User2 from "../comps/ChatComps/user2";
+import Input from "../comps/ChatComps/input";
+import { getRoomates } from "../api/room.api";
+import { globalContext } from "../store/globalContext";
+import { requireAuthen } from "../api/require.authen";
 
 const MainCont = styled.div`
-  display:flex;
-  width:100vw;
-  height:100vh;
-
-`
-
+  display: flex;
+  width: 100vw;
+  height: 100vh;
+`;
 
 //left cont
 const LeftCont = styled.div`
@@ -25,161 +24,198 @@ flex-direction: column;
 flex:2;
 border-right: 1px #D6D6D6 solid;
 di
-`
+`;
 const TopCont = styled.div`
-display:flex;
-justify-content: space-between;
-margin: 40px 75px 0px 75px;
-align-items:center;
-`
+  display: flex;
+  justify-content: space-between;
+  margin: 40px 75px 0px 75px;
+  align-items: center;
+`;
 const Heading = styled.div`
-font-size: 34px;
-font-weight: 700;
-
-`
-const Icon = styled.img`
-
-`
+  font-size: 34px;
+  font-weight: 700;
+`;
+const Icon = styled.img``;
 
 //Chat Nav cont
 const NavCont = styled.div`
-display:flex;
-width: 100%;
-height: 100%;
-flex-direction: column;
-align-items:center;
+  display: flex;
+  width: 100%;
+  height: 100%;
+  flex-direction: column;
+  align-items: center;
+`;
 
-`
-
-
-// Right Container 
+// Right Container
 
 const RightCont = styled.div`
-display:flex;
-flex:3;
-width: 100%;
-height: 100%;
-flex-direction: column;
-justify-content: flex-end;
-`
+  display: flex;
+  flex: 3;
+  width: 100%;
+  height: 100%;
+  flex-direction: column;
+  justify-content: flex-end;
+`;
 const ChatCont1 = styled.div`
-display:flex;
-width: 100%;
-`
+  display: flex;
+  width: 100%;
+`;
 const ChatCont2 = styled.div`
-display:flex;
-width: 100%;
-justify-content: flex-end;
-`
+  display: flex;
+  width: 100%;
+  justify-content: flex-end;
+`;
 
-
-
-export default function Chat() {
+export default function Chat(props) {
+  const {
+    currentUser,
+    setCurrentUser,
+    currentError,
+    setCurrentError,
+    setCurrentMsg,
+    currentMsg,
+  } = useContext(globalContext);
   const [buttonstate5, setButtonState5] = useState(0);
-  const GlobalNavClick = () =>{
-    if (buttonstate5===0){
-    setButtonState5(1);
-  }else{
-    setButtonState5(0);
-  }
-  }
+
+  const GlobalNavClick = () => {
+    if (buttonstate5 === 0) {
+      setButtonState5(1);
+    } else {
+      setButtonState5(0);
+    }
+  };
   const [buttonstate6, setButtonState6] = useState(0);
-  const NotificationClick = () =>{
-      if (buttonstate6===0){
+  const NotificationClick = () => {
+    if (buttonstate6 === 0) {
       setButtonState6(1);
       setButtonState7(0);
-    }else{
+    } else {
       setButtonState6(0);
       setButtonState7(1);
     }
+  };
+
+  const [buttonstate7, setButtonState7] = useState(0);
+  const BackClick = () => {
+    if (buttonstate7 === 0) {
+      setButtonState7(1);
+      setButtonState6(0);
+    } else {
+      setButtonState7(0);
+      setButtonState6(1);
     }
-  
-    const [buttonstate7, setButtonState7] = useState(0);
-    const BackClick = () =>{
-        if (buttonstate7===0){
-        setButtonState7(1);
-        setButtonState6(0);
-      }else{
-        setButtonState7(0);
-        setButtonState6(1);
-      }
-      }
+  };
   const [buttonstate1, setButtonState1] = useState(0);
-  const HandleClickButtonColor1 = () =>{
+  const HandleClickButtonColor1 = () => {
     setButtonState1(1);
-  } 
-  const HandleClickButtonColor2 = () =>{
+  };
+  const HandleClickButtonColor2 = () => {
     setButtonState1(2);
-  } 
-  const HandleClickButtonColor3 = () =>{
+  };
+  const HandleClickButtonColor3 = () => {
     setButtonState1(3);
-  } 
-  const HandleClickButtonColor4 = () =>{
+  };
+  const HandleClickButtonColor4 = () => {
     setButtonState1(4);
-  } 
-  const HandleClickButtonColor5 = () =>{
+  };
+  const HandleClickButtonColor5 = () => {
     setButtonState1(5);
-  } 
-  
+  };
+
+  const getChats = () => {
+    console.log("props.users props.users props.users");
+    // console.log(props);
+    // console.log(props.chatters);
+    // console.log(props.auth);
+    const chat_list = props.chatters.map((chatter) => {
+      if (chatter.id !== props.auth.user.id) {
+        return <ChatNav chatterInfo={chatter} user={props.auth.user} />;
+      }
+    });
+    return chat_list;
+  };
+
   return (
-  
     <MainCont>
-           <NavBar3 />
-        <LeftCont>
+      <NavBar3 />
+      <LeftCont>
         <TopCont>
-        <Heading className="ubuntu">Chat</Heading>
-        <Icon src="/chat.svg"/>
+          <Heading className="ubuntu">Chat</Heading>
+          <Icon src="/chat.svg" />
         </TopCont>
 
         <NavCont>
-        <ChatNav onClick={() =>{
-            HandleClickButtonColor1();
-          }} 
-          bgcolor={
-            buttonstate1 === 1 ? '#FAFAFA' : '#FFFFFF'}/>
+          {getChats()}
+          {/* <ChatNav
+            onClick={() => {
+              HandleClickButtonColor1();
+            }}
+            bgcolor={buttonstate1 === 1 ? "#FAFAFA" : "#FFFFFF"}
+          />
 
-        <ChatNav  info="Darleen, Deavon"  onClick={() =>{
-        HandleClickButtonColor2();
-      }} 
-      bgcolor={buttonstate1 === 2 ? '#FAFAFA' : '#FFFFFF'}/>
+          <ChatNav
+            info="Darleen, Deavon"
+            onClick={() => {
+              HandleClickButtonColor2();
+            }}
+            bgcolor={buttonstate1 === 2 ? "#FAFAFA" : "#FFFFFF"}
+          />
 
-        <ChatNav info="Floyd Miles" display="none" marginleft="40px"onClick={() =>{
-        HandleClickButtonColor3();
-      }} 
-      bgcolor={
-        buttonstate1 === 3 ? '#FAFAFA' : '#FFFFFF'}
-        />
-        <ChatNav  info="Devon Lane" display="none" marginleft="40px"
-        onClick={() =>{
-            HandleClickButtonColor4();
-          }} 
-          bgcolor={
-            buttonstate1 === 4 ? '#FAFAFA' : '#FFFFFF'}/>
-        
+          <ChatNav
+            info="Floyd Miles"
+            display="none"
+            marginleft="40px"
+            onClick={() => {
+              HandleClickButtonColor3();
+            }}
+            bgcolor={buttonstate1 === 3 ? "#FAFAFA" : "#FFFFFF"}
+          />
+          <ChatNav
+            info="Devon Lane"
+            display="none"
+            marginleft="40px"
+            onClick={() => {
+              HandleClickButtonColor4();
+            }}
+            bgcolor={buttonstate1 === 4 ? "#FAFAFA" : "#FFFFFF"}
+          /> */}
         </NavCont>
-        </LeftCont>
+      </LeftCont>
 
-        {/* Right Container */}
-        <RightCont>
-    
-          <ChatCont1>
+      {/* Right Container */}
+      <RightCont>
+        <ChatCont1>
           <User1></User1>
-          </ChatCont1>
-    
-          <ChatCont2>
-            <User2></User2>
-          </ChatCont2>
-        
-    
+        </ChatCont1>
+
+        <ChatCont2>
+          <User2></User2>
+        </ChatCont2>
+
         <Input></Input>
-      
-
-
-        </RightCont>
-        
-
-      
+      </RightCont>
     </MainCont>
-  
-  )
+  );
 }
+
+export const getServerSideProps = async (ctx) => {
+  // return requireAuthen(context);
+  // return getRoomates(ctx);
+
+  let authProp = await requireAuthen(ctx, true);
+
+  console.log("55555555555555555");
+  console.log(authProp);
+  let chatters = await getRoomates(ctx);
+
+  if (!authProp.hasOwnProperty("user")) {
+    return { redirect: authProp };
+  } else {
+    return {
+      props: {
+        auth: authProp,
+        chatters: chatters.props.users,
+      },
+    };
+  }
+};
